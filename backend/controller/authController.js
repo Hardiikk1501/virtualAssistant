27e826c31adbd952fbac9b1d12d8f1cba46464d1
hardik <hp15012006@gmail.com>
2026-06-17 23:53:20 +0530

    update cookie

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -33,8 +33,8 @@ export const signUp = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure:true,
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 1000, // 7 days
         });
     return res.status(201).json({ message: 'User created successfully' });
@@ -87,8 +87,8 @@ export const login = async (req, res) => {
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure:true,
+      sameSite:"none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
