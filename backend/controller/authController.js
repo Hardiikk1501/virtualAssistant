2e6c681e33f7baeba5cc2eadc16194916b806d9d
hardik <hp15012006@gmail.com>
2026-06-18 18:42:39 +0530

    #

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -29,14 +29,14 @@ export const signUp = async (req, res) => {
         await newUser.save();
         res.status(201).json({ message: 'User created successfully' });
           
-        const token = await generateToken(newUser._id);
+       // const token = await generateToken(newUser._id);
 
-        res.cookie('token', token, {
-            httpOnly: true,
-            secure:true,
-            sameSite:"none",
-            maxAge:7 * 24 * 60 * 60 * 1000, // 7 days
-        });
+        // res.cookie('token', token, {
+        //     httpOnly: true,
+        //     secure:true,
+        //     sameSite:"none",
+        //     maxAge:7 * 24 * 60 * 60 * 1000, // 7 days
+        // });
     return res.status(201).json({ message: 'User created successfully' });
     
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,8 +19,8 @@ function App() {
       <Route path="/customize" element={userData ?<Customize />:<Navigate to={"/signup"} />} />
       <Route path="/customize2" element={userData ?<Customize2 />:<Navigate to={"/signup"} />} />
 
-      <Route path="/signup" element={ !userData?<SignUp /> : <Navigate to={"/"} />} />
-      <Route path="/signin" element={ !userData ? <SignIn /> : <Navigate to={"/"} />} />
+      <Route path="/signup" element={ !userData?<SignUp /> : <Navigate to={"/customize"} />} />
+      <Route path="/signin" element={ !userData ? <SignIn /> : <Navigate to={"/customize"} />} />
     </Routes>
   )
 }
